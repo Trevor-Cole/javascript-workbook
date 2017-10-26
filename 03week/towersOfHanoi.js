5565dee6fa2fc4cf7f4ad0ac5874bb79a1761b81
Trevor Charnquist <tcharnquist@gmail.com>
2017-10-26 18:33:26 -0500

    Just comments

--- 03week/towersOfHanoi.js
+++ 03week/towersOfHanoi.js
@@ -18,26 +18,47 @@ function printStacks() {
   console.log("b: " + stacks.b);
   console.log("c: " + stacks.c);
 }
-
+//MOVE PIECE///////////////////////////////////////////////////////////////////////////
 function movePiece() {
   // Your code here
 
-}
+} 
+// Use pop to remove piece from origin stack and use push(?) to place it on destination stack.
 
+
+//IS LEGAL/////////////////////////////////////////////////////////////////////////////
 function isLegal() {
   // Your code here
 
 }
+// Use a if greater (>) than for pieces.
+// If origin piece is greater than destination piece, invalid move, otherwise, go ahead and move.
 
+//CHECK FOR WIN//////////////////////////////////////////////////////////////////////////
 function checkForWin() {
   // Your code here
 
 }
+// Is array 'b' or 'c' full? 
+// a:
+// b:4,3,2,1
+// c:
 
+// (or)
+
+// a:
+// b:
+// c:4,3,2,1
+// Use --- if array b or c is equal to [4,3,2,1] then return WIN
+
+//TOWERS OF HANOI////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 function towersOfHanoi(startStack, endStack) {
   // Your code here
 
 }
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 function getPrompt() {
   printStacks();
@@ -49,6 +70,9 @@ function getPrompt() {
   });
 }
 
+
 getPrompt();
+
+
 
 
